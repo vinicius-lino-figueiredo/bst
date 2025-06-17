9c705c73ff25b25fc48da20abb5e45dde688e82a
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2025-06-17 19:17:27 -0300

    fix(DefaultCheckValueEquality): remove non-comparable comparison
    Remove posible comparison for non-comparable types by adding a type
    switch with known comparable types

--- customutils/customutils.go
+++ customutils/customutils.go
@@ -2,6 +2,8 @@ package customutils
 
 import (
 	"fmt"
+	"reflect"
+	"time"
 )
 
 // DefaultCompareKeysFunction will work for numbers, strings and dates
@@ -22,5 +24,63 @@ func DefaultCompareKeysFunction(a any, b any) int {
 // DefaultCheckValueEquality checks whether two values are equal (used in
 // non-unique deletion)
 func DefaultCheckValueEquality(a, b any) bool {
-	return a == b
+	if a == nil {
+		if b == nil {
+			return true
+		}
+		return false
+	}
+	if b == nil {
+		return false
+	}
+	switch v := a.(type) {
+	case int:
+		return compareUnknownType(v, b)
+	case int8:
+		return compareUnknownType(v, b)
+	case int16:
+		return compareUnknownType(v, b)
+	case int32:
+		return compareUnknownType(v, b)
+	case int64:
+		return compareUnknownType(v, b)
+	case uint:
+		return compareUnknownType(v, b)
+	case uint8:
+		return compareUnknownType(v, b)
+	case uint16:
+		return compareUnknownType(v, b)
+	case uint32:
+		return compareUnknownType(v, b)
+	case uint64:
+		return compareUnknownType(v, b)
+	case uintptr:
+		return compareUnknownType(v, b)
+	case float32:
+		return compareUnknownType(v, b)
+	case float64:
+		return compareUnknownType(v, b)
+	case string:
+		return compareUnknownType(v, b)
+	case time.Time:
+		if b == nil {
+			return false
+		}
+		if bt, ok := b.(time.Time); ok {
+			return v.Equal(bt)
+		}
+		return false
+	default:
+		return reflect.ValueOf(a).Pointer() == reflect.ValueOf(b).Pointer()
+	}
+}
+
+func compareUnknownType[T comparable](a T, b any) bool {
+	if b == false {
+		return false
+	}
+	if bt, ok := b.(T); ok {
+		return a == bt
+	}
+	return false
 }
